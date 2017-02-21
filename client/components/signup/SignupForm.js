81bf6218dbf1b788989cc4fb554a8038b38017de
leobeta2 <leobeta2@gmail.com>
2017-02-20 23:59:51 -0300

    se agrego redux, y se pudo hacer un post

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import timezones from '../../data/timezones'
 import map from 'lodash/map'
-import axios from 'axios'
+//import axios from 'axios'
 
 class SignupForm extends React.Component {
      constructor(props)  {
@@ -26,7 +26,8 @@ class SignupForm extends React.Component {
      onSubmit(e) {
          e.preventDefault();
          //peticion a una api ocn axios
-         axios.post('/api/users', { user:this.state });
+         //axios.post('/api/users', { user:this.state });
+         this.props.userSignupRequest(this.state);
      }
 
     render() {
@@ -96,6 +97,10 @@ class SignupForm extends React.Component {
             </form>
         );
     }
+}
+
+SignupForm.propTypes = {
+    userSignupRequest: React.PropTypes.func.isRequired
 }
 
 export default SignupForm
